0788428f9d3fb7be1f641a5af342c1ea86f3d2d5
wilberchair <wilberchair@gmail.com>
2023-03-07 21:40:05 -0300

    criando funcao para botao e trabalhando com callback

--- callback.js
+++ callback.js
@@ -1,1 +1,28 @@
-console.log('oi')
+const newButton = (text, callback) => {
+  const body = document.querySelector('body');
+  const button = document.createElement('button');
+  button.textContent = text;
+  
+  callback(button);
+  
+  body.insertAdjacentElement('beforeend', button);
+
+  return button;
+};
+
+const btnLogin = newButton('Login', (button) => {
+  button.style.cssText = `
+    color: red;
+    font-size:20px
+  `;
+});
+const btnSignup = newButton('Sing Up', (button) => {
+  button.style.cssText = `
+    color: blue;
+    font-size: 30px
+  `;
+});
+
+// button.addEventListener('click', () => {
+//   alert('oi')
+// })
