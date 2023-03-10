8b1907df564bae7a4cfeb3481295100a6324a30a
wilberchair <wilberchair@gmail.com>
2023-03-09 22:36:23 -0300

    reduzindo mais codigo desestruturando

--- pokemons/pokemons.js
+++ pokemons/pokemons.js
@@ -3,16 +3,16 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
   fetch(getPokemonUrl(index + 1)).then(response => response.json()))
   
-const generateHtml = pokemons => pokemons.reduce((accumulator, pokemon) => {
-  const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+const generateHtml = pokemons => pokemons.reduce((accumulator, {name, id, types}) => {
+  const elementTypes = types.map(typeInfo => typeInfo.type.name);
   
   accumulator += 
     `<li class="card">
       <img 
-      class="card-img ${types[0]}" 
-      alt="${pokemon.name}"       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
-        <h2 class="card-title">${pokemon.id}- ${pokemon.name}</h2>
-        <p class="card-subtitle">${types.join(' | ')}</p>
+      class="card-img ${elementTypes[0]}" 
+      alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"/>
+        <h2 class="card-title">${id}- ${name}</h2>
+        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
     </li>`
   return accumulator;
 }, '');
@@ -27,5 +27,3 @@ const pokemonPromises = generatePokemonPromises();
 Promise.all(pokemonPromises)
   .then(generateHtml)
   .then(insertPokemonsIntoPage);
-
-fetchPokemon();
