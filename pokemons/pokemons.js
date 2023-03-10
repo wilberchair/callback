412453675a30ed0e26cb6fed50132ff2efb42071
wilberchair <wilberchair@gmail.com>
2023-03-09 22:23:28 -0300

    refatorando codigo

--- pokemons/pokemons.js
+++ pokemons/pokemons.js
@@ -1,37 +1,34 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
+  fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+  
+const generateHtml = pokemons => {
+  return pokemons.reduce((accumulator, pokemon) => {
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+    
+    accumulator += 
+      `<li class="card">
+        <img 
+        class="card-img ${types[0]}" 
+        alt="${pokemon.name}"       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+          <h2 class="card-title">${pokemon.id}- ${pokemon.name}</h2>
+          <p class="card-subtitle">${types.join(' | ')}</p>
+      </li>`
+    return accumulator;
+}, '')
+}
+
+const insertPokemonsIntoPage = pokemons => {
+  const ul = document.querySelector('.ul-pokemons');
+  ul.innerHTML = pokemons;
+}
+
 const fetchPokemon = () => {
-  const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
-    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
-    const pokemonPromises = generatePokemonPromises();
+  const pokemonPromises = generatePokemonPromises();
 
-  
-  // for(let i = 1; i <= 150; i++) {
-  //   pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
-  // }
-  
   Promise.all(pokemonPromises)
-    .then(pokemons => {
-    // console.log(pokemons);
-    
-    const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-      const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-      accumulator += 
-        `<li class="card">
-          <img 
-          class="card-img ${types[0]}" 
-          alt="${pokemon.name}"       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
-            <h2 class="card-title">${pokemon.id}- ${pokemon.name}</h2>
-            <p class="card-subtitle">${types.join(' | ')}</p>
-         </li>`
-      return accumulator;
-    }, '')
-    
-    console.log(lisPokemons);
-    const ul = document.querySelector('.ul-pokemons');
-    ul.innerHTML = lisPokemons;
-
-  });
+    .then(generateHtml).then(insertPokemonsIntoPage);
 }
 
 fetchPokemon();
