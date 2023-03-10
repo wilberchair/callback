180f6731d1896f61a009deca5af9bd1f6fb0a6fb
wilberchair <wilberchair@gmail.com>
2023-03-09 22:33:37 -0300

    identacao

--- pokemons/pokemons.js
+++ pokemons/pokemons.js
@@ -4,17 +4,17 @@ const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
   fetch(getPokemonUrl(index + 1)).then(response => response.json()))
   
 const generateHtml = pokemons => pokemons.reduce((accumulator, pokemon) => {
-    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-    
-    accumulator += 
-      `<li class="card">
-        <img 
-        class="card-img ${types[0]}" 
-        alt="${pokemon.name}"       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
-          <h2 class="card-title">${pokemon.id}- ${pokemon.name}</h2>
-          <p class="card-subtitle">${types.join(' | ')}</p>
-      </li>`
-    return accumulator;
+  const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+  
+  accumulator += 
+    `<li class="card">
+      <img 
+      class="card-img ${types[0]}" 
+      alt="${pokemon.name}"       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+        <h2 class="card-title">${pokemon.id}- ${pokemon.name}</h2>
+        <p class="card-subtitle">${types.join(' | ')}</p>
+    </li>`
+  return accumulator;
 }, '');
 
 const insertPokemonsIntoPage = pokemons => {
